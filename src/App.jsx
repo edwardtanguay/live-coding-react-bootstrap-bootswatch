ba51b8d1715638037d61a3bd72a0bf1aef8b97f5
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-03 10:06:56 +0200

    bootswatch is in

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/bootstrap.darkly.min.css';
+// import './styles/bootstrap.morph.min.css';
+// import './styles/bootstrap.quartz.min.css';
 import { Container, Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div>
-      <Container>
+      <Container className="pt-5">
        <h1>Company Site</h1> 
        <Button className="me-1">Employees</Button>
        <Button className="btn-success">Customers</Button>
