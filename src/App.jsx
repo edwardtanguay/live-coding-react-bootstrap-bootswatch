a7cbd5120a2a6d73b88fc5857106b3031162cef3
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-03 10:10:05 +0200

    card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@
 import './styles/bootstrap.darkly.min.css';
 // import './styles/bootstrap.morph.min.css';
 // import './styles/bootstrap.quartz.min.css';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Card } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div>
-      <Container className="pt-5">
-       <h1>Company Site</h1> 
-       <Button className="me-1">Employees</Button>
-       <Button className="btn-success">Customers</Button>
-       </Container>
-    </div>
-  )
+    return (
+        <div>
+            <Container className="pt-5">
+                <h1>Company Site</h1>
+                <Button className="me-2">Employees</Button>
+                <Button className="btn-success">Customers</Button>
+                <Card className="mt-2">
+                    <Card.Body>This is some text within a card body.</Card.Body>
+                </Card>
+            </Container>
+        </div>
+    );
 }
 
-export default App
+export default App;
