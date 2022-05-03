d8eab68f46a4938dde57ec893e294ff88ba2508a
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-03 13:29:17 +0200

    error fixed

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -3,7 +3,7 @@ export const Employees = ({ employees }) => {
 		<>
 			<h2>Employees</h2>
 			<p>This is the employees page.</p>
-			<ul></ul>
+			<ul>
 				{employees.map((emp, index) => {
 					return <li key={index}>{emp.firstName} {emp.lastName}</li>;
 				})}
