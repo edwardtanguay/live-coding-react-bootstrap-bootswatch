dc09ee8760ab054febfc892c822844448af428cf
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-03 10:20:13 +0200

    buttons work

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/bootstrap.darkly.min.css';
 // import './styles/bootstrap.morph.min.css';
@@ -6,21 +7,31 @@ import { Container, Button, Card } from 'react-bootstrap';
 import { Employees } from './components/Employees';
 import { Customers } from './components/Customers';
 
+const handleEmployeeButtonClick = () => {
+	console.log('employee button clicked');
+};
+
+const handleCustomerButtonClick = () => {
+	console.log('customer button clicked');
+};
+
 function App() {
-    return (
-        <div>
-            <Container className="pt-5">
-                <h1>Company Site</h1>
-                <Button className="me-2">Employees</Button>
-                <Button className="btn-success">Customers</Button>
-                <Card className="mt-2">
-                    <Card.Body>
-                        <Customers />
-                    </Card.Body>
-                </Card>
-            </Container>
-        </div>
-    );
+	return (
+		<div>
+			<Container className="pt-5">
+				<h1>Company Site</h1>
+				<Button onClick={handleEmployeeButtonClick} className="me-2">
+					Employees
+				</Button>
+				<Button onClick={handleCustomerButtonClick} className="btn-success">Customers</Button>
+				<Card className="mt-2">
+					<Card.Body>
+						<Customers />
+					</Card.Body>
+				</Card>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
